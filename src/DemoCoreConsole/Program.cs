2eaa29bd69a386d1bfc2aa5cc7c147e0af54f89a
Akira Inoue <chack.inoue@hotmail.com>
2016-05-20 21:20:41 +0900

    プロジェクト ファイルを追加します。

---
+++ src/DemoCoreConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DemoCoreConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int index = 1;
+            if (args.Length > 0)
+                index = int.Parse(args[0]);
+
+            Task<string> dt = Util.GetData(index);
+            dt.Wait();
+
+            Console.WriteLine(dt.Result);
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/DemoCoreConsole/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCoreConsole
+{
+    public class Util
+    {
+        public static async Task<string> GetData(int index)
+        {
+            string url = "http://decode16dataapi.azurewebsites.net/data" + index.ToString() + ".txt";
+            var client = new System.Net.Http.HttpClient();
+            return await client.GetStringAsync(url);
+        }
+        public static string GetDataPre(int index)
+        {
+            Task<string> dt = Util.GetData(index);
+            dt.Wait();
+            return "<pre>" + dt.Result.Replace("\r\n", "<br />") + "</pre>";
+        }
+    }
+}
